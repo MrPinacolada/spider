6dd61b610ad910496910095be28650b70b2a00ae
MrPinacolada <vasiasko112@gmail.com>
2024-04-05 16:22:35 +0300

    add segment class

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,72 @@ window.onload = function initAnimation() {
   let canvas = document.getElementById("recluse-spider") as HTMLCanvasElement,
     c = canvas?.getContext("2d"),
     w = (canvas.width = window.innerWidth),
-    h = (canvas.height = window.innerHeight);
+    h = (canvas.height = window.innerHeight),
+    mouse = { x: false, y: false },
+    last_mouse = {};
 
-  function handleMouseMove(e: MouseEvent) {}
+  const dist = (p1x: number, p1y: number, p2x: number, p2y: number) => {
+    return Math.sqrt(Math.pow(p2x - p1x, 2) + Math.pow(p2y - p1y, 2));
+  };
 
-  function handleMouseLeave(e: MouseEvent) {}
+  class segment {
+    first: any;
+    pos: { x: any; y: any };
+    l: any;
+    ang: any;
+    nextPos: { x: any; y: any };
+    constructor(
+      parent: { x: any; y: any; nextPos: { x: any; y: any } },
+      l: any,
+      a: any,
+      first: any
+    ) {
+      this.first = first;
+      if (first) {
+        this.pos = {
+          x: parent.x,
+          y: parent.y,
+        };
+      } else {
+        this.pos = {
+          x: parent.nextPos.x,
+          y: parent.nextPos.y,
+        };
+      }
+      this.l = l;
+      this.ang = a;
+      this.nextPos = {
+        x: this.pos.x + this.l * Math.cos(this.ang),
+        y: this.pos.y + this.l * Math.sin(this.ang),
+      };
+    }
+    update(t: { y: number; x: number }) {
+      this.ang = Math.atan2(t.y - this.pos.y, t.x - this.pos.x);
+      this.pos.x = t.x + this.l * Math.cos(this.ang - Math.PI);
+      this.pos.y = t.y + this.l * Math.sin(this.ang - Math.PI);
+      this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+      this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+    }
+    fallback(t: { x: any; y: any }) {
+      this.pos.x = t.x;
+      this.pos.y = t.y;
+      this.nextPos.x = this.pos.x + this.l * Math.cos(this.ang);
+      this.nextPos.y = this.pos.y + this.l * Math.sin(this.ang);
+    }
+    show() {
+      c?.lineTo(this.nextPos.x, this.nextPos.y);
+    }
+  }
 
-  function handleMouseDown(e: MouseEvent) {}
+  const handleMouseMove = (e: MouseEvent) => {};
 
-  function handleMouseUp(e: MouseEvent) {}
+  const handleMouseLeave = (e: MouseEvent) => {};
 
-  function handleResize() {}
+  const handleMouseDown = (e: MouseEvent) => {};
+
+  const handleMouseUp = (e: MouseEvent) => {};
+
+  const handleResize = () => {};
 
   canvas.addEventListener("mousemove", handleMouseMove, false);
   canvas.addEventListener("mouseleave", handleMouseLeave);
