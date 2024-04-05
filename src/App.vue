bbccb77f6b8eb8a6eb0d1c285c38ec41081e1b33
MrPinacolada <vasiasko112@gmail.com>
2024-04-05 10:22:36 +0300

    add canvas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
-  <RouterView />
+  <canvas id="recluse-spider"></canvas>
 </template>
 
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+window.onload = function initAnimation() {
+  let canvas = document.getElementById("recluse-spider") as HTMLCanvasElement,
+    c = canvas?.getContext("2d"),
+    w = (canvas.width = window.innerWidth),
+    h = (canvas.height = window.innerHeight);
+
+  function handleMouseMove(e: MouseEvent) {}
+
+  function handleMouseLeave(e: MouseEvent) {}
+
+  function handleMouseDown(e: MouseEvent) {}
+
+  function handleMouseUp(e: MouseEvent) {}
+
+  function handleResize() {}
+
+  canvas.addEventListener("mousemove", handleMouseMove, false);
+  canvas.addEventListener("mouseleave", handleMouseLeave);
+  canvas.addEventListener("mousedown", handleMouseDown, false);
+  canvas.addEventListener("mouseup", handleMouseUp, false);
+  window.addEventListener("resize", handleResize);
+};
 </script>
 
-<style scoped></style>
+<style>
+body,
+html {
+  margin: 0px;
+  padding: 0px;
+  position: fixed;
+  background: rgb(30, 30, 30);
+}
+</style>
